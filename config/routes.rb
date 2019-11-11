e4542be0517692e1396f6c6d6318cb202576978f
Peter Ayeni <ayenicology@gmail.com>
2019-11-11 18:52:45 +0000

    Complete advanced setup

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   get 'static_pages/contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static_pages#hello'
+  root 'static_pages#home'
 end
