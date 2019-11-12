37577bbe469591ffd184e4763f022ef142ba8879
Peter Ayeni <ayenicology@gmail.com>
2019-11-12 15:37:03 +0000

    Created title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def full_title(page_name = '')
+    base_title = 'Ruby on Rails Tutorial Sample App'
+    if page_name.empty?
+      base_title
+    else
+      "#{page_name} | #{base_title}"
+    end
+  end
 end
